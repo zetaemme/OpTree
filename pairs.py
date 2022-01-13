a8d7227032e7daf5e9818d98fe8de10e0d40d683
Mattia Zorzan <mattia.zorzan@gmail.com>
2022-01-13 17:48:20 +0100

    Finished the Pairs class
    The Pairs class accepts a pandas.DataFrame and automatically counts
    the number of pairs in the dataset and collects them.
    It creates a list of tuples with all of them as __post_init__.
    The number is set to be the length of the pair_list field
    
    Signed-off-by: zetaemme <mattia.zorzan@gmail.com>

--- pairs.py
+++ pairs.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from pandas import DataFrame
 
@@ -6,6 +6,20 @@ from pandas import DataFrame
 @dataclass
 class Pairs:
     dataset: DataFrame
+    number: int = field(init=False)
+    pair_list: list[tuple[int]] = field(init=False)
 
-    def __post_init__(self):
-        pass
+    def __post_init__(self) -> None:
+        """Inits the number field"""
+        # We suppose to have a 'class' column in the dataset
+        assert 'class' in self.dataset.columns, 'Dataset should contain a \'class\' column'
+
+        # FIXME: Convert the list init to a list comprehension
+        for i1, l1 in enumerate(self.dataset):
+            for i2, l2 in enumerate(self.dataset[i1:]):
+                if l2['class'] is not None and l1['class'] != l2['class']:
+                    pair = tuple([i1, i2])
+                    if (i2, i1) not in self.pair_list:
+                        self.pair_list.append(pair)
+
+        self.number = len(self.pair_list)
