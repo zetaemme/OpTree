5510d971f842fd99241cb4994f303c609c62e523
zetaemme <mattia.zorzan@gmail.com>
2022-01-14 11:36:18 +0100

    Fixed and optimized Pairs class
    Now the pair_list __post_init__ is a list comprehension and the dataset field became __dataset.
    Iteration over it has been optimized by the iterrows() method and a redundant check has been removed from the if.
    
    Signed-off-by: zetaemme <mattia.zorzan@gmail.com>

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import pandas as pd
+
 from pairs import Pairs
 
 
 def main():
-    # FIXME: Pass the dataset as a pandas DataFrame
-    data_pairs = Pairs()
+    """The main function"""
+    dataset = pd.DataFrame(
+        # The "dataset" at page 3 of the paper
+        data=[
+            [1, 1, 2, 'A', 0.1],
+            [1, 2, 1, 'A', 0.2],
+            [2, 2, 1, 'B', 0.4],
+            [1, 2, 2, 'C', 0.25],
+            [2, 2, 2, 'C', 0.05],
+        ],
+        columns=['t1', 't2', 't3', 'class', 'probability']
+    )
+
+    pairs = Pairs(dataset)
+
+    print(pairs)
 
 
 if __name__ == '__main__':
--- pairs.py
+++ pairs.py
@@ -5,21 +5,20 @@ from pandas import DataFrame
 
 @dataclass
 class Pairs:
-    dataset: DataFrame
+    __dataset: DataFrame = field(repr=False)
     number: int = field(init=False)
-    pair_list: list[tuple[int]] = field(init=False)
+    pair_list: list[tuple] = field(init=False, default_factory=list)
 
     def __post_init__(self) -> None:
         """Inits the number field"""
         # We suppose to have a 'class' column in the dataset
-        assert 'class' in self.dataset.columns, 'Dataset should contain a \'class\' column'
+        assert 'class' in self.__dataset.columns, 'Dataset should contain a \'class\' column'
 
-        # FIXME: Convert the list init to a list comprehension
-        for i1, l1 in enumerate(self.dataset):
-            for i2, l2 in enumerate(self.dataset[i1:]):
-                if l2['class'] is not None and l1['class'] != l2['class']:
-                    pair = tuple([i1, i2])
-                    if (i2, i1) not in self.pair_list:
-                        self.pair_list.append(pair)
+        self.pair_list = [
+            tuple([i1, i2])
+            for i1, d1 in self.__dataset.iterrows()
+            for i2, d2 in self.__dataset[i1:].iterrows()
+            if d1['class'] != d2['class']
+        ]
 
         self.number = len(self.pair_list)
