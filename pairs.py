1eee0c231e27ebd00b684a6c32e63d8872e7babc
zetaemme <mattia.zorzan@gmail.com>
2022-01-14 17:43:08 +0100

    removed redundant tuple() call in __post_init__()
    Signed-off-by: zetaemme <mattia.zorzan@gmail.com>

--- pairs.py
+++ pairs.py
@@ -10,12 +10,12 @@ class Pairs:
     pair_list: list[tuple] = field(init=False, default_factory=list)
 
     def __post_init__(self) -> None:
-        """Inits the number field"""
+        """Inits the number and pair_list fields"""
         # We suppose to have a 'class' column in the dataset
         assert 'class' in self.__dataset.columns, 'Dataset should contain a \'class\' column'
 
         self.pair_list = [
-            tuple([i1, i2])
+            (i1, i2)
             for i1, d1 in self.__dataset.iterrows()
             for i2, d2 in self.__dataset[i1:].iterrows()
             if d1['class'] != d2['class']
