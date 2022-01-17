5f115e59a8bb4f83c65a270fbb1c1f412573847d
zetaemme <mattia.zorzan@gmail.com>
2022-01-17 14:25:03 +0100

    Finished the Adapted-Greedy heuristic implementation
    The tk variable is the k-th element of the tests array
    
    Signed-off-by: zetaemme <mattia.zorzan@gmail.com>

--- heuristic.py
+++ heuristic.py
@@ -1,19 +1,20 @@
-from typing import Callable, NewType
+from typing import Callable, NewType, TypeAlias
 
 from pandas import DataFrame
 
 from test import Test
 
 uint = NewType('uint', int)
+TestList: TypeAlias = list[Test]
 
 
 def adapted_greedy(
         objects: DataFrame,
-        tests: list[Test],
+        tests: TestList,
         f: Callable,
-        cost_f: Callable[[Test], uint],
+        cost_fn: Callable[[Test], uint],
         bound: uint
-) -> list[Test]:
+) -> TestList:
     """Implementation of the Adapted-Greedy heuristic"""
     assert bound >= 0, 'Bound should be a positive integer!'
 
@@ -22,16 +23,18 @@ def adapted_greedy(
     k = 0
 
     # Remove from T all tests with cost larger than B
-    tests = [t for t in tests if cost_f(t) <= bound]
+    tests = [t for t in tests if cost_fn(t) <= bound]
 
     if not tests:
         while True:
             k += 1
 
-            # TODO: Implement 'Let tests[k] be...'
-
             tests.pop(k)
-            spent += cost_f(tests[k])
+
+            cost = cost_fn(tests[k])
+            assert cost >= 0, 'Cost should be a positive value!'
+            spent += cost
+
             A.append(tests[k])
 
             if spent > bound or not tests:
