330c9a95098f73353f6c33f8b9efa4398d905cd7
zetaemme <mattia.zorzan@gmail.com>
2022-01-18 15:51:56 +0100

    Continued algorithm implementation
    Signed-off-by: zetaemme <mattia.zorzan@gmail.com>

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from sys import argv
 
 import pandas as pd
 
-from cost import Cost
+from cost import calculate_cost, find_budget
 from dectree.dectree import DecTree
 from dectree.node import LeafNode, TestNode
 from dectree.test import Test
 from pairs import Pairs
 
 
-def main(tests_filepath: str):
+def main(tests_filepath: str, classes_filepath: str):
     """The main function"""
     dataset = pd.DataFrame(
         # The "dataset" at page 3 of the paper
@@ -29,12 +29,17 @@ def main(tests_filepath: str):
         raw_tests = [line.rstrip() for line in f]
         tests = [Test(test) for test in raw_tests]
 
+    with open(classes_filepath, 'r', encoding='UTF-8') as f:
+        raw_classes = [line.rstrip() for line in f]
+        classes = [class_str for class_str in raw_classes]
+
+    # FIXME: Completare la definizione della classe Cost
+    test_costs = [calculate_cost(test) for test in tests]
+
     if pairs.number == 0:
         return DecTree(LeafNode(dataset[0]['class']))
 
     if pairs.number == 1:
-        # FIXME: Completare la definizione della classe Cost
-        test_costs = [Cost.calculate(test) for test in tests]
         minimum_cost_test = min(test_costs)
 
         # FIXME: L'init di LeafNode è osceno
@@ -47,8 +52,17 @@ def main(tests_filepath: str):
             )
         )
 
-    # TODO: Continuare da FindBudget
+    budget = find_budget(dataset, tests, classes, calculate_cost)
+    spent = 0
+    spent2 = 0
+    # U <- S
+    universe = dataset
+    k = 1
+
+    while any([test for index, test in enumerate(tests) if test_costs[index] <= budget - spent]):
+        # TODO: Continuare da 'Let tk be a test...'
+        pass
 
 
 if __name__ == '__main__':
-    main(argv[1])
+    main(argv[1], argv[2])
