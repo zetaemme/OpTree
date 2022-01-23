067ab3c6aaf664487bfd8d36423ff9e899806a4f
zetaemme <mattia.zorzan@gmail.com>
2022-01-23 19:28:12 +0100

    Added API to calculate separation cost
    Work in progress
    
    Signed-off-by: zetaemme <mattia.zorzan@gmail.com>

--- src/cost.py
+++ src/cost.py
@@ -1,15 +1,24 @@
-from typing import Callable
+from typing import Callable, TypeAlias
 
 from pandas import DataFrame
 
 from dectree.test import Test
 
+TestList: TypeAlias = list[Test]
+
 
 # FIXME: Modificare con una funzione di costo accettabile.
-#        Per ora è stata utilizzata la variante con costo 1/2 del paper
+#        Per ora è stata utilizzata la variante con costo 1/2 del paper (testing), dovrebbe tornare int
 def calculate_cost(test: Test) -> float:
     """Calculates the cost of a given test"""
     return 0.5
+
+
+# FIXME: Dovrebbe tornare int, ma si adatta ll'attuale cost_fn
+def calculate_separation_cost(tests: TestList) -> float:
+    """Calculates the separation cost of a given test list"""
+    # TODO: Ritorna sepcost come da paper
+    pass
 
 
 # TODO: Implementare come da paper
