9bc57bf8c9063a790b961e7cbf44b34c5d8295b5
zetaemme <mattia.zorzan@gmail.com>
2022-01-23 19:28:56 +0100

    Added API to extract the minimum sepcost test
    Work in progress
    
    Signed-off-by: zetaemme <mattia.zorzan@gmail.com>

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,20 @@
+from typing import TypeAlias
+
 from pandas import DataFrame
+
+from src.dectree.test import Test
+
+TestList: TypeAlias = list[Test]
+
+
+def extract_cheapest_test(tests: TestList, objects: tuple) -> Test:
+    """Extracts the cheapest (separation cost) test that separates the two objects"""
+    assert len(objects) == 2, "The tuple should contain only 2 items!"
+
+    if len(tests) == 1:
+        return tests[0]
+
+    # TODO: Ritornare il test con sepcost minimo
 
 
 def extract_object_class(dataset: DataFrame, index: int) -> str:
