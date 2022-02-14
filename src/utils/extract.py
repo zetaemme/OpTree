5448fa1013cccb668a387fec88fbe2e650061ee5
zetaemme <mattia.zorzan@gmail.com>
2022-02-14 12:25:57 +0100

    Created the utils package
    This package is used to store all the functions that doesn't fit in the classes.
    
    Signed-off-by: zetaemme <mattia.zorzan@gmail.com>

---
+++ src/utils/extract.py
@@ -0,0 +1,33 @@
+from typing import TypeAlias
+
+from pandas import DataFrame
+
+from src.cost import calculate_cost
+from src.dectree.test import Test
+
+TestList: TypeAlias = list[Test]
+
+
+def cheapest_test(tests: TestList) -> Test:
+    """Extracts the cheapest (separation cost) test that separates the two objects"""
+    if len(tests) == 1:
+        return tests[0]
+
+    if all(calculate_cost(test) == 1 for test in tests):
+        return tests[0]
+
+    # TODO: Aggiungere return statement per test costs effettivi
+
+
+def object_class(dataset: DataFrame, index: int) -> str:
+    """Extracts the class label from the item in position index of a given dataset"""
+    assert index >= 0, "Index should be a positive integer"
+    return dataset[index]['class']
+
+
+def test_structure(test: str) -> Test:
+    """Extracts the test structure (lhs, type, rhs) from a given string"""
+    structure = test.split()
+
+    # FIXME: Usare tipo corretto per lhs e rhs
+    return Test(structure[0], structure[1], structure[2])
