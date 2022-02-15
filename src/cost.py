a7c62cec2e0e0c91147bd043b267d159ce6b4f85
zetaemme <mattia.zorzan@gmail.com>
2022-02-15 19:40:31 +0100

    Started find_budget implementation.
    Continue from "Do a binary search..."
    
    Signed-off-by: zetaemme <mattia.zorzan@gmail.com>

--- src/cost.py
+++ src/cost.py
@@ -2,24 +2,39 @@ from typing import Callable, TypeAlias
 
 from pandas import DataFrame
 
-from dectree.test import Test
+from src.dectree.test import Test
+from src.pairs import Pairs
 
 TestList: TypeAlias = list[Test]
 
 
 def calculate_cost(test: Test) -> int:
     """Calculates the cost of a given test"""
+    # FIXME: Settare a vera funzione di costo, 1 messo solo per testing
     return 1
 
 
-# FIXME: Dovrebbe tornare int, ma si adatta ll'attuale cost_fn
-def calculate_separation_cost(tests: TestList) -> float:
-    """Calculates the separation cost of a given test list"""
-    # TODO: Ritorna sepcost come da paper
-    pass
-
-
 # TODO: Implementare come da paper
-def find_budget(objects: DataFrame, tests: list[Test], classes: set[str], cost_fn: Callable[[Test], float]):
+def find_budget(objects: DataFrame, tests: list[Test], classes: set[str], cost_fn: Callable[[Test], float]) -> int:
     """Implementation of the FindBudget procedure of the referenced paper"""
-    pass
+
+    def submod_f1(sub_tests: TestList):
+        pairs = Pairs(objects)
+
+        items_separated_by_test = [
+            item
+            for test in sub_tests
+            for index, _ in enumerate(classes)
+            for item in test.evaluate_dataset_for_class(objects, index)
+        ]
+
+        items_separated_by_test = list(set(items_separated_by_test))
+
+        sep_pairs = Pairs(DataFrame(items_separated_by_test))
+
+        return pairs.number - sep_pairs.number
+
+    alpha = 0.35
+
+    # TODO: Continuare da "Do a binary search..."
+    return 0
