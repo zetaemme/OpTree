94359b44b250befb6d91a7b00dee4438411831db
zetaemme <mattia.zorzan@gmail.com>
2022-03-10 14:15:02 +0100

    Node now keeps track of his parents and his depth
    To do this, a constructor parameter has been added.
    Since a Node now supports multiple outcomes, the label generation method has been reimplemented using a simple regex.
    
    Signed-off-by: zetaemme <mattia.zorzan@gmail.com>

--- src/dectree/node.py
+++ src/dectree/node.py
@@ -1,3 +1,4 @@
+import re
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from numbers import Number
@@ -10,8 +11,10 @@ from src.utils import extract
 @dataclass
 class Node(ABC):
     """An Abstract Base Class for the Node class"""
-    label: str = field(init=True)
-    children: list[Union['TestNode', 'LeafNode']] = field(default=None)
+    label: str
+    children: list[Union['TestNode', 'LeafNode']] = field(default_factory=list)
+    depth: int = field(init=False)
+    parent: 'TestNode' = None
 
     @abstractmethod
     def add_children(self,
@@ -28,10 +31,10 @@ class TestNode(Node):
     __test: Test = field(init=False)
 
     def __post_init__(self) -> None:
-        assert 0 < len(self.children) <= 2, 'TestNodes must have 1 or 2 children!'
         self.__test = extract.test_structure(self.label)
-        # Removes the outcomes from the node label after initializing the test
-        self.label = self.label[:-4]
+        # Removes the outcomes from the node label after initializing the test using a regex match
+        self.label = re.match('^((?:\S+\s+){2}\S+).*', self.label).group(1)
+        self.depth = self.parent.depth + 1 if self.children else 0
 
     def add_children(self,
                      children: Union[Union['TestNode', 'LeafNode'], Sequence[Union['TestNode', 'LeafNode']]]) -> None:
@@ -47,6 +50,7 @@ class LeafNode(Node):
 
     def __post_init__(self) -> None:
         assert not self.children, 'LeafNodes shouldn\'t have any child!'
+        self.depth = self.parent.depth + 1 if self.children else 0
 
     def outcome(self, lhs_value: Optional[Number]) -> str:
         """Returns the string associated with the class ot the leaf"""
