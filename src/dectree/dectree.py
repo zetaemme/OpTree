dc3d71fbe5945794924c0265a4e6a71ed9c5e391
zetaemme <mattia.zorzan@gmail.com>
2022-03-10 14:08:32 +0100

    Removed unused Iterator implementation
    Since no direct traverse of the tree is needed, implementing an iterator is no longer needed.
    To improve readability, the Type NodeType has been removed to express the type as a simple Union.
    
    Signed-off-by: zetaemme <mattia.zorzan@gmail.com>

--- src/dectree/dectree.py
+++ src/dectree/dectree.py
@@ -1,22 +1,10 @@
 from dataclasses import dataclass
-from typing import Iterator, Type
+from typing import Union
 
-from node import Node
-
-NodeType: Type['Node']
+from src.dectree.node import LeafNode, TestNode
 
 
 @dataclass
 class DecTree:
     """Represents a Decision Tree"""
-    root: NodeType
-
-    def __iter__(self) -> Iterator[Node]:
-        """Creates an Iterator, which iterates over the nodes of a Decision Tree"""
-        self.current = self.root
-        return self
-
-    def __next__(self) -> Node:
-        """Gets the Iterator the next node of the Decision Tree"""
-        # TODO: Scegliere come deve essere tornato un nodo
-        pass
+    root: Union[LeafNode, TestNode]
