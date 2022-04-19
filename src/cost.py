11ab65a37d447fcd3804961fca8c3ca86e60c541
zetaemme <mattia.zorzan@gmail.com>
2022-04-19 16:53:11 +0200

    Updated cost.py functions signatures
    Signed-off-by: zetaemme <mattia.zorzan@gmail.com>

--- src/cost.py
+++ src/cost.py
@@ -1,11 +1,12 @@
-from pandas import DataFrame
+from typing import Callable
 
-from src.dectree.test import Test
+from pandas import DataFrame, Series
+
 from src.heuristic import adapted_greedy
 from src.pairs import Pairs
 
 
-def calculate_cost(test: Test) -> int:
+def calculate_cost(test: Series) -> int:
     """Calculates the cost of a given test
 
     Args:
@@ -15,18 +16,15 @@ def calculate_cost(test: Test) -> int:
         int: The cost of the given test
     """
     # FIXME: This value is returned for testing purpose, a real cost function must be created in the future
+    #        For the future implementation consider using ENTROPY as a cost metric
     return 1
 
 
 def find_budget(
         objects: DataFrame,
-        tests: list[Test],
-        test_costs: dict[Test, int],
+        tests: list[str],
         classes: set[str],
-        # NOTE: By computing the test cost at the beginning of the procedure, we can achieve constant lookup time
-        #       extracting the cost of a certain test.
-        #       This holds on the assumption that the cost of a test can't change during the execution.
-        # cost_fn: Callable[[Test], int],
+        cost_fn: Callable[[Series], int],
         dataset_pairs_number: int
 ) -> int:
     """Implementation of the FindBudget procedure of the referenced paper
@@ -34,7 +32,6 @@ def find_budget(
     Args:
         objects (DataFrame): The dataset we want to classify
         tests (list[Test]): The list of the tests that can be applied to the dataset
-        test_costs (dict[Test, int]): A dictionary containing, for each test, the corresponding effective cost
         classes (set[str]): A set containing all the possible classes in the dataset
         cost_fn (Callable[[Test], int]): A function that computes the effective cost of a given test
         dataset_pairs_number (int): The number of pairs in the whole dataset
@@ -43,7 +40,7 @@ def find_budget(
         int: The maximum budget that the algorithm can use to build the Decision Tree
     """
 
-    def submodular_f1(sub_tests: list[Test]):
+    def submodular_f1(sub_tests: list[str]):
         items_separated_by_test = [
             item
             for test in sub_tests
@@ -67,7 +64,7 @@ def find_budget(
         if upper >= lower:
             mid = lower + (upper - lower) / 2
 
-            heuristic_test_list = adapted_greedy(tests, test_costs, submodular_f1, mid)
+            heuristic_test_list = adapted_greedy(objects, tests, submodular_f1, calculate_cost, mid)
 
             heuristic_test_coverage_sum = sum([
                 len(test.evaluate_dataset_for_class(objects, class_index))
@@ -84,4 +81,4 @@ def find_budget(
         else:
             raise ValueError
 
-    return heuristic_binary_search(1, sum([test_costs[test] for test in tests]))
+    return heuristic_binary_search(1, sum([cost_fn(objects[test]) for test in tests]))
