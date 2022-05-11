7791d5a01770dcbabfe6679b9db3a1332da128a4
zetaemme <mattia.zorzan@gmail.com>
2022-05-11 11:39:27 +0200

    Turned Pairs into a normal class
    No benefits were given by representing a pair as a data object
    
    Signed-off-by: zetaemme <mattia.zorzan@gmail.com>

--- src/pairs.py
+++ src/pairs.py
@@ -1,9 +1,6 @@
-from dataclasses import dataclass, field
-
 from pandas import DataFrame
 
 
-@dataclass
 class Pairs:
     """A tuple of items having different classes
 
@@ -14,20 +11,16 @@ class Pairs:
     pair_list: list[tuple]
         A list containing all the pairs tuples in a given dataset
     """
-    _dataset: DataFrame = field(repr=False)
-    number: int = field(init=False)
-    pair_list: list[tuple] = field(init=False, default_factory=list)
-
-    def __post_init__(self) -> None:
+    def __init__(self, dataset: DataFrame) -> None:
         """Inits the number and pair_list fields"""
         # We suppose to have a 'class' column in the dataset
-        assert 'class' in self._dataset.columns, 'Dataset should contain a \'class\' column'
+        assert 'class' in dataset.columns, 'Dataset should contain a \'class\' column'
 
         # If item1 and item2 have a different class, they constitute a pair
         self.pair_list = [
             (i1, i2)
-            for i1, d1 in self._dataset.iterrows()
-            for i2, d2 in self._dataset[i1:].iterrows()
+            for i1, d1 in dataset.iterrows()
+            for i2, d2 in dataset[i1:].iterrows()
             if d1['class'] != d2['class']
         ]
 
