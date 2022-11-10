90cec27f24698b24d2e693a11f2f819b2f820f89
zetaemme <mattia.zorzan@gmail.com>
2022-11-10 22:28:23 +0100

    Indented and write better comment

--- src/budget.py
+++ src/budget.py
@@ -5,12 +5,17 @@ from src.utils import submodular_function_1
 
 
 def find_budget(dataset: Dataset, separation: Separation) -> float:
-    # NOTE: In the original paper alpha is marked as (1 - e^{X}), approximated with 0.35
+    # Should be (1 - e^{chi}), approximated with 0.35
     alpha = 0.35
 
     # FIXME: This should be done by Binary Search
     for budget in range(1, dataset.total_cost + 1):
-        heuristic_result = wolsey_greedy_heuristic(budget, dataset, separation, submodular_function_1)
+        heuristic_result = wolsey_greedy_heuristic(
+            budget,
+            dataset,
+            separation,
+            submodular_function_1
+        )
 
         # NOTE: Test covering -> KEEPS or SEPARATES
 
