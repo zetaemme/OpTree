25e2feed58ca2a36f4ad99728131942491aca4b6
zetaemme <mattia.zorzan@gmail.com>
2022-11-14 22:19:47 +0100

    Partially finished find_bidget implementation

--- src/budget.py
+++ src/budget.py
@@ -5,6 +5,15 @@ from src.utils import submodular_function_1
 
 
 def find_budget(dataset: Dataset, separation: Separation) -> float:
+    """Finds the optimal threshold for tests costs during decision tree creation.
+
+    Args:
+        dataset (Dataset): The dataset on which the procedure is running
+        separation (Separation): The class containing the S* and S^i sets
+
+    Returns:
+        float: The optimal budget for the decision tree test costs
+    """
     # Should be (1 - e^{chi}), approximated with 0.35
     alpha = 0.35
 
@@ -17,6 +26,10 @@ def find_budget(dataset: Dataset, separation: Separation) -> float:
             submodular_function_1
         )
 
-        # NOTE: Test covering -> KEEPS or SEPARATES
+        covered_pairs = list(
+            set(separation.kept[test] + separation.separated[test])
+            for test in heuristic_result
+        )
 
-    return 0.0
+        if len(covered_pairs) >= (alpha * dataset.pairs_number):
+            return budget
