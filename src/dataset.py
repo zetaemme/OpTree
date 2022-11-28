9739bfe3d37b4c0f0c4af208bce8fc499e50b051
zetaemme <mattia.zorzan@gmail.com>
2022-11-28 22:14:04 +0100

    Updated and optimized dataset.py to support set operations and handle observation probabilities

--- src/dataset.py
+++ src/dataset.py
@@ -18,18 +18,15 @@ class Dataset:
     class Pairs:
         """A tuple of items having different classes"""
 
-        number: int
         pairs_list: list[tuple[int, int]]
 
         def __init__(self, dataset: npt.NDArray) -> None:
-            item_classes: list[str] = dataset[:, -1, None].ravel().tolist()
+            item_classes: list[str] = dataset[:, -2, None].ravel().tolist()
 
             if dataset.shape[0] == 1:
                 self.pairs_list = []
-                self.number = 0
 
                 del item_classes
-
                 return
 
             self.pairs_list: list[tuple[int, int]] = [
@@ -41,11 +38,14 @@ class Dataset:
 
             del item_classes
 
-            self.number: int = len(self.pairs_list)
+        @property
+        def number(self) -> int:
+            return len(self.pairs_list)
 
-    features: npt.NDArray
-    probabilities: dict[str, float]
+    class_probabilities: dict[str, float]
     costs: dict[str, float]
+    features: list[str]
+    # _data: npt.NDArray
     _pairs: Pairs
     _table: npt.NDArray
 
@@ -53,10 +53,10 @@ class Dataset:
         dataset_df: pd.DataFrame = pd.read_csv(dataset_path)
         dataset_np = dataset_df.to_numpy()
 
-        self.features = dataset_df.columns.values[:-1]
+        self.features = dataset_df.columns.values[:-2].tolist()
 
         counter = Counter(dataset_np[:, -1, None].flatten().tolist())
-        self.probabilities = {
+        self.class_probabilities = {
             key: (value / sum(counter.values()))
             for key, value in counter.items()
         }
@@ -68,15 +68,20 @@ class Dataset:
             axis=0
         )
 
+        # self._data = self._table[1:]
+
         self.costs = {}
 
+        # FIXME: Cannot use variance, it sums up to 1.
+        #       To avoid the problem, we multiply by 10 the variance.
+        #       Other cost metrics should be considered!
         for idx, column_name in enumerate(self.features):
-            self.costs[column_name] = round(dataset_np[:, idx, None].var())
+            self.costs[column_name] = round(
+                dataset_np[:, idx, None].var(),
+                2
+            ) * 10
 
-        self.costs = {
-            key: value
-            for key, value in sorted(self.costs.items(), key=lambda item: item[1])
-        }
+        self.costs = dict(sorted(self.costs.items(), key=lambda item: item[1]))
 
         del counter
         del dataset_df, dataset_np
@@ -90,7 +95,7 @@ class Dataset:
 
     def get_class_of(self, idx: int) -> str:
         """Returns the class of the observation at index idx"""
-        return self._table[idx + 1][-1]
+        return self._table[idx + 1][-2]
 
     def copy(self) -> Self:
         """Returns a deep copy of the dataset"""
@@ -108,21 +113,9 @@ class Dataset:
         if complete:
             return self._table[1:]
 
-        return self._table[1:, :-1]
+        return self._table[1:, :-2]
 
-    def multi_get(self, indexes: list[int], *, complete=False) -> npt.NDArray:
-        """Returns multiple rows of the dataset
-
-        Args:
-            indexes (list[int]): The indexes to be returned
-            complete (bool, optional): States if the "class" column should be returned. Defaults to False.
-
-        Returns:
-            npt.NDArray: A subset of the rows of the dataset
-        """
-        return self.data(complete=complete)[[indexes]][0]
-
-    def from_sfeatures_subset(self, features: list[str]) -> Self:
+    def from_features_subset(self, features: list[str]) -> Self:
         """Returns a copy of the dataset, containing only the given features
 
         Args:
@@ -131,6 +124,9 @@ class Dataset:
         Returns:
             Self: An instance of the dataset containing only the given features
         """
+        if not features:
+            return self.copy()
+
         dataset_copy = self.copy()
         remaining_features = set(self.features) - set(features)
 
@@ -140,11 +136,34 @@ class Dataset:
         return dataset_copy
 
     def drop_feature(self, feature: str) -> None:
-        feature_index = np.where(self.features == feature)[0][0]
+        feature_index = self.features.index(feature)
+        print(f"{feature_index=}")
+        print("BEFORE")
+        print(self._table)
         self._table = np.delete(self.data(), feature_index, axis=1)
+        print("AFTER")
+        print(self._table)
 
-    def set_minus(self, other: npt.NDArray, axis=0) -> npt.NDArray:
-        return np.delete(self._table[1:, :-1], other, axis)
+    def drop_row(self, index: int) -> None:
+        # TODO: In future, it could be necessary to update this function to recompute objects probability after deletion
+        self._table = np.delete(self.data(), index, axis=0)
+        self._pairs.pairs_list = [
+            pair
+            for pair in self._pairs.pairs_list
+            if index not in pair
+        ]
+
+    def difference(self, other: npt.NDArray, *, axis=0) -> npt.NDArray:
+        """Computes the set difference between two datasets
+
+        Args:
+            other (npt.NDArray): The set to subtract from the dataset
+            axis (int, optional): 0 for rows, 1 for columns. Defaults to 0.
+
+        Returns:
+            npt.NDArray: the set difference between two datasets
+        """
+        return np.delete(self._table[1:, :-2], other, axis)
 
     def index_of_row(self, other: npt.NDArray) -> int | list[int]:
         """Returns the index of a given row
@@ -155,12 +174,31 @@ class Dataset:
         Returns:
             int | list[int]: The row number in the dataset
         """
-        return np.where(np.all(self.data() == other, axis=1))[0][0]
+        return np.where(np.any(self.data() == other))[0][0]
+
+    def intersection(self, other: list[int]) -> Self:
+        """Calculates the intersection between the dataset and the given subset of rows
+
+        Args:
+            other (npt.NDArray): The rows to intersect
+
+        Returns:
+            npt.NDArray: The resulting intersection
+        """
+        dataset_copy = self.copy()
+
+        data_as_set = {index for _, index in enumerate(dataset_copy.data())}
+        difference = data_as_set.symmetric_difference(set(other))
+
+        for row in difference:
+            dataset_copy.drop_row(row)
+
+        return dataset_copy
 
     @property
     def classes(self) -> list[str]:
         """Returns a list of all the possible class labels"""
-        return list(self.probabilities.keys())
+        return list(self.class_probabilities.keys())
 
     @property
     def pairs_list(self) -> list[tuple]:
@@ -176,3 +214,12 @@ class Dataset:
     def total_cost(self) -> int:
         """Returns the sum of the feature costs"""
         return int(fsum(self.costs.values()))
+
+    @property
+    def total_probability(self) -> float:
+        """Total probability of the dataset objects
+
+        Returns:
+            float: Sum of all the probabilities 
+        """
+        return fsum(probability for probability in self._table[1:, -1, None])
