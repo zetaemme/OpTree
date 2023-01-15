547c1fe854e610ac323a4a8671802ffc0ce0fdb3
zetaemme <mattia.zorzan@gmail.com>
2023-01-15 12:35:50 +0100

    Indented and applied types module for clarity

--- src/maximization.py
+++ src/maximization.py
@@ -1,8 +1,8 @@
 import logging
-from typing import Callable
 
 from src.dataset import Dataset
 from src.separation import Separation
+from src.types import SubmodularFunction
 
 logger = logging.getLogger(__name__)
 
@@ -11,11 +11,10 @@ def probability_maximization(universe: Dataset, budget: float, spent: float) -> 
     universe_separation = Separation(universe)
 
     def calculate_probability_maximization_for(feature: str) -> float:
-        intersection = universe.intersection(
-            universe_separation.S_star[feature]
-        )
-        return (universe.total_probability - intersection.total_probability)\
-            / universe.costs[feature]
+        intersection = universe.intersection(universe_separation.S_star[feature])
+        return (
+            universe.total_probability - intersection.total_probability
+        ) / universe.costs[feature]
 
     maximum_eligible: dict[str, float] = {
         feature: calculate_probability_maximization_for(feature)
@@ -30,10 +29,10 @@ def pairs_maximization(universe: Dataset) -> str:
     universe_separation = Separation(universe)
 
     def calculate_pairs_maximization_for(feature: str) -> float:
-        intersection = universe.intersection(
-            universe_separation.S_star[feature]
-        )
-        return (universe.pairs_number - intersection.pairs_number) / universe.costs[feature]
+        intersection = universe.intersection(universe_separation.S_star[feature])
+        return (universe.pairs_number - intersection.pairs_number) / universe.costs[
+            feature
+        ]
 
     maximum_eligible: dict[str, float] = {
         feature: calculate_pairs_maximization_for(feature)
@@ -47,7 +46,7 @@ def submodular_maximization(
     dataset: Dataset,
     separation,
     features: list[str],
-    submodular_function: Callable[[Dataset, Separation, list[str]], int]
+    submodular_function: SubmodularFunction,
 ) -> str:
     logger.info(f"Maximizing submodular function for {features}")
     maximum_eligible: dict[str, float] = {}
@@ -57,8 +56,7 @@ def submodular_maximization(
         union_result = submodular_function(dataset, separation, features)
         features.remove(feature)
 
-        submodular_result = (union_result - feature_result) \
-            / dataset.costs[feature]
+        submodular_result = (union_result - feature_result) / dataset.costs[feature]
 
         maximum_eligible[feature] = submodular_result
 
