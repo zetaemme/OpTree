747351e2cabd6ef18b55a16ff32fc632ee90f04a
zetaemme <mattia.zorzan@gmail.com>
2023-01-17 22:39:15 +0100

    Fix binary search lower bound

--- src/budget.py
+++ src/budget.py
@@ -20,7 +20,7 @@ def find_budget(dataset: Dataset, separation: Separation) -> float:
         float: The optimal budget for the decision tree test costs
     """
     logger.info("Starting budget computation")
-    search_bounds = Bounds(0.0, dataset.total_cost)
+    search_bounds = Bounds(1.0, dataset.total_cost)
     return binary_search_budget(
         dataset, separation, search_bounds, wolsey_greedy_heuristic
     )
