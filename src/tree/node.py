1b3776808be2028cb8d9e475f85ad08d4609cb03
zetaemme <mattia.zorzan@gmail.com>
2023-01-21 20:03:05 +0100

    Add custom tree daat structure

--- src/tree/node.py
+++ src/tree/node.py
@@ -1,6 +1,27 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Self
 
 
 @dataclass
 class Node:
+    """A node in the decision tree."""
+
     label: str
+    parent: Self | None = field(default=None)
+    children: list[Self] = field(init=False, default_factory=list)
+
+    def add_child(self, node: Self) -> None:
+        """Adds the given node as child of this node.
+
+        Args:
+            node (Self): The node to be added as child.
+        """
+        self.children.append(node)
+
+    def set_parent(self, parent: Self) -> None:
+        """Updates the parent of this node.
+
+        Args:
+            parent (Self): The node to be set as parent.
+        """
+        self.parent = parent
--- src/tree/tree.py
+++ src/tree/tree.py
@@ -5,5 +5,29 @@ from src.tree.node import Node
 
 @dataclass
 class Tree:
+    """The decision tree."""
+
     root: Node = field(init=False)
-    children: list[Node] = field(init=False, default_factory=list)
+    intermediate_nodes: list[Node] = field(default_factory=list)
+
+    def add_node(self, node: Node, *, parent_label=None) -> None:
+        """Adds the given node as a child of the labelled node, if given as parameter.
+        If parent_label is not passed as parameter, the node will be added as child of root.
+
+        Args:
+            node (Node): The node to be added.
+            parent_label (str, optional): The label of the given node's parent. Defaults to None.
+        """
+        if parent_label is not None:
+            target_node = self.find_child_labelled(parent_label)
+            node.set_parent(target_node)
+            target_node.add_child(node)
+            self.intermediate_nodes.append(target_node)
+        else:
+            node.set_parent(self.root)
+            self.root.add_child(node)
+
+    def find_child_labelled(self, label: str) -> Node:
+        """Finds the intermediate node labelled with the given label"""
+        child = [node for node in self.intermediate_nodes if node.label == label]
+        return child[0]
