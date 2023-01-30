f0c7e7657b3657e9e5af054afc68ba8d97bca5b8
zetaemme <mattia.zorzan@gmail.com>
2023-01-30 23:12:53 +0100

    Format main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ logging.basicConfig(
     format="%(asctime)s [%(levelname)s] - OpSion @ {%(module)s.py -> %(funcName)s} - %(message)s",
     datefmt="%H:%M:%S",
 )
+logger = logging.getLogger(__name__)
 
 
 def main(dataset_path: str) -> None:
@@ -24,18 +25,16 @@ def main(dataset_path: str) -> None:
 
     decision_tree = build_decision_tree(dataset, separation)
 
+    logger.info("Done!")
+
     if decision_tree is not None:
         with open("model/decision_tree.obj", "wb") as obj_file:
             dump(decision_tree, obj_file, HIGHEST_PROTOCOL)
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser(
-        prog="main.py", description="Builds (log-)optimal decision trees"
-    )
-    parser.add_argument(
-        "-f", "--filename", type=str, help="The CSV file containing the dataset"
-    )
+    parser = ArgumentParser(prog="main.py", description="Builds (log-)optimal decision trees")
+    parser.add_argument("-f", "--filename", type=str, help="The CSV file containing the dataset")
 
     args = parser.parse_args()
     main(args.filename)
