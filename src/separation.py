4ef722cc3267903b8da9a443233c795b7a2b435c
zetaemme <mattia.zorzan@gmail.com>
2023-01-31 22:59:48 +0100

    Adapt to dataset.py new structure

--- src/separation.py
+++ src/separation.py
@@ -13,6 +13,8 @@ logger = logging.getLogger(__name__)
 
 @dataclass(init=False)
 class Separation:
+    _all_features: list[str]
+
     S_label: dict[str, dict[Any, list[int]]] = field(default_factory=dict)
     S_star: dict[str, list[int]] = field(default_factory=dict)
     sigma: dict[str, list[int]] = field(default_factory=dict)
@@ -28,44 +30,42 @@ class Separation:
         self.kept = {}
         self.separated = {}
 
-        for feature_idx, feature in enumerate(dataset.features):
+        self._all_features = dataset.features
+
+        for feature_idx, feature in enumerate(self._all_features):
             self.S_label[feature] = {
-                value: [
-                    obj_idx
-                    for obj_idx, obj_value in enumerate(dataset.data())
-                    if obj_value[feature_idx] == value
-                ]
-                for value in {value[0] for value in dataset.data()[:, feature_idx, None]}
+                value: [observation[0] for observation in dataset.data() if observation[feature_idx + 1] == value]
+                for value in set(dataset.data()[:, feature_idx + 1])
             }
 
             feature_pairs = {
-                label: dataset.pairs_number_for(objects)
-                for label, objects in self.S_label[feature].items()
+                label: dataset.pairs_number_for(objects) for label, objects in self.S_label[feature].items()
             }
-
-            max_pairs = max(feature_pairs, key=feature_pairs.get)
+            max_pairs = max(feature_pairs, key=feature_pairs.get)  # type: ignore
             self.S_star[feature] = self.S_label[feature][max_pairs]
 
-            # FIXME: Alla terza iterazione dell'euristica si ferma qui
-            self.sigma[feature] = [
-                dataset.index_of_row(row)
-                for row in dataset.difference(self.S_star[feature])
-            ]
+            self.sigma[feature] = [row[0] for row in dataset.difference(self.S_star[feature])]
 
             # FIXME: Find a better way to do this (itertools)
-            self.kept[feature] = list({
-                pair
-                for pair in dataset.pairs_list
-                if pair[0] in self.sigma[feature] and pair[1] in self.sigma[feature]
-            })
+            self.kept[feature] = list(
+                {
+                    pair
+                    for pair in dataset.pairs_list
+                    if pair[0] in self.sigma[feature] and pair[1] in self.sigma[feature]
+                }
+            )
 
             # FIXME: Find a better way to do this (itertools)
-            self.separated[feature] = list({
-                pair
-                for pair in dataset.pairs_list
-                if pair[0] in self.sigma[feature] and pair[1] in self.S_star[feature]
-                or pair[1] in self.sigma[feature] and pair[0] in self.S_star[feature]
-            })
+            self.separated[feature] = list(
+                {
+                    pair
+                    for pair in dataset.pairs_list
+                    if pair[0] in self.sigma[feature]
+                       and pair[1] in self.S_star[feature]
+                       or pair[1] in self.sigma[feature]
+                       and pair[0] in self.S_star[feature]
+                }
+            )
 
     def __getitem__(self, key: str) -> dict[Any, list[int]]:
         return self.S_label[key]
@@ -75,12 +75,8 @@ class Separation:
 
         for idx, key1 in enumerate(key_list):
             for key2 in key_list[idx:]:
-                if (
-                        obj_idx1 in self.S_label[feature][key1] and
-                        obj_idx2 in self.S_label[feature][key2]
-                ) or (
-                        obj_idx1 in self.S_label[feature][key2] and
-                        obj_idx2 in self.S_label[feature][key1]
+                if (obj_idx1 in self.S_label[feature][key1] and obj_idx2 in self.S_label[feature][key2]) or (
+                        obj_idx1 in self.S_label[feature][key2] and obj_idx2 in self.S_label[feature][key1]
                 ):
                     return True
 
@@ -91,7 +87,8 @@ class Separation:
     def for_features_subset(self, features: list[str]) -> Self:
         separation_copy = deepcopy(self)
 
-        for feature in features:
+        to_delete = set(self._all_features).difference(features)
+        for feature in to_delete:
             del separation_copy.S_label[feature]
             del separation_copy.S_star[feature]
             del separation_copy.sigma[feature]
