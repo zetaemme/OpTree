87a7cc322dd9e96f8d5de36df6fa4b68a9df4953
zetaemme <mattia.zorzan@gmail.com>
2023-02-18 11:59:34 +0100

    Implement benchmark mode

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import logging
-from argparse import ArgumentParser
+from argparse import ArgumentParser, BooleanOptionalAction
 from os.path import dirname
 from pathlib import Path
 from pickle import HIGHEST_PROTOCOL, dump
+from timeit import timeit
 
-from src import DEBUG
 from src.dataset import Dataset
 from src.decision_tree import build_decision_tree
 
@@ -16,8 +16,14 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+def benchmark_main() -> None:
+    path = Path("data/test.csv")
+    dataset = Dataset(path)
+    build_decision_tree(dataset)
+
+
 def main(dataset_path: str) -> None:
-    """Inits dataset and test list in order to pass them to the algorithm"""
+    """Inits dataset and runs the algorithm"""
     path = Path(dirname(__file__) + "/" + dataset_path)
     dataset = Dataset(path)
 
@@ -26,14 +32,21 @@ def main(dataset_path: str) -> None:
     logger.info("Done!")
     decision_tree.print()
 
-    if decision_tree is not None:
-        with open("model/decision_tree.obj", "wb") as obj_file:
-            dump(decision_tree, obj_file, HIGHEST_PROTOCOL)
+    dataset_name = dataset_path.replace('data/', '').replace('.csv', '')
+    with open(f"model/decision_tree_{dataset_name}.obj", "wb") as obj_file:
+        dump(decision_tree, obj_file, HIGHEST_PROTOCOL)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(prog="main.py", description="Builds (log-)optimal decision trees")
+    parser.add_argument("-b", "--benchmark", action=BooleanOptionalAction)
     parser.add_argument("-f", "--filename", type=str, help="The CSV file containing the dataset")
 
     args = parser.parse_args()
-    main(args.filename)
+
+    if args.benchmark:
+        logging.disable(logging.INFO)
+        time = timeit(stmt="benchmark_main()", globals=globals(), number=1000)
+        print(f"Time: {time / 1000} seconds")
+    else:
+        main(args.filename)
